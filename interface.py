e73c0e88b60e84fe59be915df203abe0e43e9033
evgeniyvspb <119848258+evgeniyvspb@users.noreply.github.com>
2023-09-12 14:25:29 +0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -3,23 +3,24 @@ from metods import create_note, read_note, update_note, delete_note, print_all
 
 def menu_notes(full_name: str):
         print('Меню справочника: 1- Add note, 2 - Read, 3 - Update, 4 - Delete 5 - Print All')
-        # full_name = "notes.csv"
-        # notes_dir = dict()
-        # key_count =1
         while True:    
                 num = int(input("Введите желаемую операцию:   "))
                 if num!=1 and num!=2 and num!=3 and num!=4 and num!=5 and num!=6 and num!=7:
+                        print('Неверный ввод номера действия. Возможно повезёт в следующий раз')
                         break
                 if num==1:
                         create_note(full_name)
                 if num==2:
-                        number_pos = int(input("Введите позицию:   ")) #будет время трайкеч сделать
+                        try: number_pos = int(input("Введите позицию:   ")) #будет время трайкеч сделать
+                        except: print('Ошибка! Неверный формат ввода номера позиции')
                         read_note(number_pos, full_name)
                 if num==3:
-                        number_pos = int(input("Введите позицию:   ")) #будет время трайкеч сделать
+                        try: number_pos = int(input("Введите позицию:   ")) #будет время трайкеч сделать
+                        except: print('Ошибка! Неверный формат ввода номера позиции')
                         update_note(number_pos, full_name)
                 if num==4:
-                        number_pos = int(input("Введите позицию:   ")) #будет время трайкеч сделать
+                        try: number_pos = int(input("Введите позицию:   ")) #будет время трайкеч сделать
+                        except: print('Ошибка! Неверный формат ввода номера позиции')
                         delete_note(number_pos, full_name)
                         # key_count-1
                 if num==5:
