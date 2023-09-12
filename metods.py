a33957ad7ac03ce506a2aa3331e92baf9368a556
evgeniyvspb <119848258+evgeniyvspb@users.noreply.github.com>
2023-09-12 12:44:56 +0300

    Update metods.py

--- metods.py
+++ metods.py
@@ -5,47 +5,50 @@ from os.path import join,abspath,dirname, exists
 
 def create_note(full_name: str):
     user = list()
-    with open(full_name, mode = "a+", encoding = "utf-8") as file:
+    with open(full_name, mode = "r+", encoding = "utf-8") as file:
         lines = file.readlines()
-        keynum = lines.count+1
+        if len(lines)>0:
+            keynum = int(lines[len(lines)-1][0])+1  # берем последнюю строчку и смотрим первый элемент
+        else: keynum = 1  # берем последнюю строчку и смотрим первый элемент
         user.append(str(keynum))
         user.append(input("Введите заголовок:   "))
         user.append(input("Введите саму заметку (тело):    "))
         user.append(str(datetime.now()))
-        user.append(str(datetime.today()))
         with open(full_name, mode = "a", encoding = "utf-8") as file:
-            # for idc, user in phone_dir.items():
-            file.write(f"{user[0]}; {user[1]}; {user[2]};{user[3]};{user[4]}\n")
+            file.write(f"{user[0]}; {user[1]}; {user[2]};{user[3]}\n")
  
 def read_note(keynum: int, full_name: str):
     if not exists(full_name):
         print("Ошибка программы! Нужно сначала создать запись перед тем как что-то прочитать")
         return
-    file1 = open(full_name, "r")
-    while True:
+    with open(full_name,'r') as file:
     # считываем строку
-        line = file1.readline(keynum)
-    # прерываем цикл, если строка пустая
-        if not line:
-            break
-    print(line.strip())
-    file1.close
-    return line
+        lines =file.readlines()
+        line = lines[keynum-1]
+    print(line.strip("  "))
+    return line    
 
 def update_note (keynum: int, full_name: str):
     line =read_note(keynum, full_name)
-    lst = line.strip().split(';')
-    lst[1] = lst.append(input("Введите новый заголовок:   "))
-    lst[2] = lst.append(input("Введите новую запись:   "))
-    lst[3] = lst.append(str(datetime.now))
+    with open(full_name, mode = "r", encoding = "utf-8") as file:
+        lines = file.readlines()
+    lst=[4]
+    lst[0]=str(keynum)
+    lst.append(input("Введите новый заголовок:   "))
+    lst.append(input("Введите новую запись:   "))
+    lst.append(str(datetime.now()))
+    lines[keynum-1] = lst[0]+";"+ str(lst[1])+";"+ str(lst[2]) +";"+ str(lst[3])+ "\n"
+    with open(full_name, mode = "w", encoding = "utf-8") as file:
+            for line in lines:
+                file.write(line)
 
 def delete_note (keynum: int, full_name: str):
-    with open(full_name,'w') as old:
-        lines = old.readlines()
-        if (lines.count<=keynum): 
-            print("Такой позиции в списке нет. Извините и попробуйте еще раз")
-        lines.pop(keynum)
-        old.writelines(lines)
+    with open(full_name, mode = "r", encoding = "utf-8") as file:
+        lines = file.readlines()
+    lines.pop(keynum-1)
+    with open(full_name, mode = "w", encoding = "utf-8") as file:
+            for line in lines:
+                file.write(line)
 
 def print_all(file_name: str):
     file1 = open(file_name, "r")
@@ -58,6 +61,4 @@ def print_all(file_name: str):
     # выводим строку
         print(line.strip()+"\n")
     # закрываем файл
-    file1.close        
-
-
+    file1.close     
